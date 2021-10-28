721c585ebd56509ee2c7cbcc9ab39e4cf18aaeac
luca-heltai <luca-heltai@users.noreply.github.com>
2021-10-28 07:37:37 +0000

    deploy: 23d0631ac9a0b1b5d5aa9ae7d582a6ce0d100705

--- struct_reference_inclusion.js
+++ struct_reference_inclusion.js
@@ -2,7 +2,9 @@ var struct_reference_inclusion =
 [
     [ "ReferenceInclusion", "struct_reference_inclusion.html#a81d68745ec7516acd950c5e7b5807818", null ],
     [ "get_current_support_points", "struct_reference_inclusion.html#ab6033822a3b3d82a1d4d58bdb672a119", null ],
-    [ "get_dof_indices", "struct_reference_inclusion.html#a484d5d875745d3bddf2fa358a297bdbb", null ],
+    [ "get_dof_indices", "struct_reference_inclusion.html#ac7ea47c32210429cab121e387b6e9480", null ],
+    [ "get_inclusion_id", "struct_reference_inclusion.html#a3674f7767f268211104c9a1ae8d44710", null ],
+    [ "get_normal", "struct_reference_inclusion.html#a03cdc7bc03dd31992eea6412dae32f09", null ],
     [ "reinit", "struct_reference_inclusion.html#a6578f14a201e4458e610a86b95b8d527", null ],
     [ "current_center", "struct_reference_inclusion.html#aa586504133b2204b9895dc10af5c68fb", null ],
     [ "current_fe_values", "struct_reference_inclusion.html#a1f26cb86e615a204456a15f955695052", null ],
@@ -11,6 +13,7 @@ var struct_reference_inclusion =
     [ "current_support_points", "struct_reference_inclusion.html#a2b883ed1c0ed5d027344d60acd404934", null ],
     [ "n_coefficients", "struct_reference_inclusion.html#ad0fd9a2184f63892a8933b7a7ebadee7", null ],
     [ "n_q_points", "struct_reference_inclusion.html#a9bf26e3b079aef4d977dd178f75c89dd", null ],
+    [ "normals", "struct_reference_inclusion.html#a9df0bf877cfc7a5ba44f9b236a2f8407", null ],
     [ "support_points", "struct_reference_inclusion.html#a440736315869f8829673e381e8c9c6bd", null ],
     [ "theta", "struct_reference_inclusion.html#abced9c0a9cc00a82a615925d8b20d943", null ]
 ];
