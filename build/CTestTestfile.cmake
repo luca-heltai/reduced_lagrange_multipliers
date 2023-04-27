05ce7205499302bef41d7a131fe5fd5d99724d2f
belponer <belponer@wias-berlin.de>
2023-04-27 13:02:45 +0200

    clean

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/CODICE_lagrangian
-# Build directory: /workspaces/CODICE_lagrangian/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
-subdirs("gtests")
