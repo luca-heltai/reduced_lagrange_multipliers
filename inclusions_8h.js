ad8b718e6b0484017b24f2d9c91752947590d394
luca-heltai <luca-heltai@users.noreply.github.com>
2024-11-07 10:33:30 +0000

    deploy: 09be93ad45b9315b6f494cd0deebc9756553a2fc

--- inclusions_8h.js
+++ inclusions_8h.js
@@ -1,4 +1,5 @@
 var inclusions_8h =
 [
-    [ "Inclusions< spacedim >", "class_inclusions.html", "class_inclusions" ]
+    [ "Inclusions< spacedim >", "class_inclusions.html", "class_inclusions" ],
+    [ "to_lower_copy", "inclusions_8h.html#a980574d61586c2f7c02b302a9cda4710", null ]
 ];
---
+++ index.js
@@ -0,0 +1,8 @@
+var index =
+[
+    [ "Overview", "index.html#autotoc_md0", null ],
+    [ "Features", "index.html#autotoc_md1", null ],
+    [ "Installation", "index.html#autotoc_md2", null ],
+    [ "Online documentation", "index.html#autotoc_md3", null ],
+    [ "Licence", "index.html#autotoc_md4", null ]
+];
