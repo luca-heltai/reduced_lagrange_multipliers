2fe868e241fe84b4184ef6f35af8835ee0cb64da
Luca Heltai <luca.heltai@unipi.it>
2024-11-27 18:50:06 +0100

    Update docker and download 1d solver library.

--- scripts/dr.sh
+++
@@ -1,5 +0,0 @@
-IMG=`grep -m 1 image .gitlab-ci.yml | awk '{print $2}'` 
-echo $IMG
-docker run  --user $(id -u):$(id -g) \
-    --rm -t \
-    -v `pwd`:/builds/app $IMG /bin/sh -c "cd /builds/app; $@"
