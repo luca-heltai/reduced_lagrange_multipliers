09c5f31367d584e88781c55477383a97785f9544
Marco Feder <marco.feder@sissa.it>
2025-04-29 16:53:42 +0200

    Distributed serial vector across processors and add a test

--- gtests/vtk_utils_01.cc
+++ gtests/vtk_utils_01.cc
@@ -1,7 +1,18 @@
-#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/base/mpi.h>
+#include <deal.II/base/point.h>
 
+#include <deal.II/distributed/tria.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/mapping_q1.h>
+
+#include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/tria.h>
 
+#include <deal.II/lac/la_parallel_vector.h>
 #include <deal.II/lac/vector.h>
 
 #include <string>
@@ -10,6 +21,7 @@
 #include "gtest/gtest.h"
 #include "vtk_utils.h"
 
+namespace LA = dealii::LinearAlgebra;
 using namespace dealii;
 
 #ifdef DEAL_II_WITH_VTK
@@ -67,6 +79,95 @@ TEST(VTKUtils, ReadVtkDH)
   EXPECT_EQ(data_names[1], "path_distance");
   EXPECT_EQ(output_vector.size(), 19);
   std::cout << "output vector norm: " << output_vector.l2_norm() << std::endl;
+}
+
+TEST(VTKUtils, MPI_FillDistributedVectorFromSerial)
+{
+  const int          dim       = 2;
+  const unsigned int fe_degree = 1;
+  FE_Q<dim>          fe(fe_degree);
+  MappingQ1<dim>     mapping;
+
+
+  // Setup serial tria
+  Triangulation<dim> serial_tria;
+  GridGenerator::hyper_cube(serial_tria, 0, 1);
+  serial_tria.refine_global(2);
+
+  DoFHandler<dim> serial_dof_handler(serial_tria);
+  serial_dof_handler.distribute_dofs(fe);
+
+  // Build support point map (serial)
+  std::map<types::global_dof_index, Point<dim>> serial_support_points;
+  DoFTools::map_dofs_to_support_points(mapping,
+                                       serial_dof_handler,
+                                       serial_support_points);
+  std::map<Point<dim>, types::global_dof_index, VTKUtils::PointComparator<dim>>
+    serial_map;
+  for (const auto &pair : serial_support_points)
+    serial_map[pair.second] = pair.first;
+
+
+  // Fill serial vector with position-dependent values
+  Vector<double> serial_vec(serial_dof_handler.n_dofs());
+  for (const auto &pair : serial_map)
+    {
+      const Point<dim>             &pt           = pair.first;
+      const types::global_dof_index serial_index = pair.second;
+
+      // Calculate value based on point coordinates
+      double value             = 10.0 * pt[0] + 1. * pt[1];
+      serial_vec[serial_index] = value;
+    }
+
+
+  // Parallel tria, it must be identical to the one I generated above
+
+  parallel::distributed::Triangulation<dim> parallel_tria(MPI_COMM_WORLD);
+  GridGenerator::hyper_cube(parallel_tria, 0, 1);
+  parallel_tria.refine_global(2);
+
+  DoFHandler<dim> parallel_dof_handler(parallel_tria);
+  parallel_dof_handler.distribute_dofs(fe);
+
+  // Build support point map (parallel)
+
+  std::map<types::global_dof_index, Point<dim>> parallel_support_points;
+  DoFTools::map_dofs_to_support_points(mapping,
+                                       parallel_dof_handler,
+                                       parallel_support_points);
+  std::map<Point<dim>, types::global_dof_index, VTKUtils::PointComparator<dim>>
+    parallel_map;
+  for (const auto &pair : parallel_support_points)
+    parallel_map[pair.second] = pair.first;
+
+  // Fill parallel vector using the utility function
+  LA::distributed::Vector<double> parallel_vec;
+  VTKUtils::fill_distributed_vector_from_serial(parallel_dof_handler,
+                                                serial_vec,
+                                                serial_map,
+                                                mapping,
+                                                parallel_vec,
+                                                parallel_map,
+                                                MPI_COMM_WORLD);
+
+
+  // Check values in distributed vector
+  for (const auto &p_pair : parallel_map)
+    {
+      const auto &pt             = p_pair.first;
+      const auto &parallel_index = p_pair.second;
+
+      if (!parallel_dof_handler.locally_owned_dofs().is_element(parallel_index))
+        continue;
+
+      // Calculate expected value based on point coordinates
+      double expected_value = 10.0 * pt[0] + 1.0 * pt[1];
+
+      // Check if vector has correct value
+      ASSERT_NEAR(parallel_vec[parallel_index], expected_value, 1e-10)
+        << "Mismatch at point " << pt;
+    }
 }
 
 #endif // DEAL_II_WITH_VTK
