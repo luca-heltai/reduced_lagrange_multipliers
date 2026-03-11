8fa261f69eccf148cd2daffb3d93b1cc8884d4cb
belponer <camilla.belponer@gmail.com>
2026-03-11 19:24:55 +0100

    added test for exact solution of lambda (wip)

--- gtests/elasticity_01.cc
+++ gtests/elasticity_01.cc
@@ -349,57 +349,57 @@ TEST(ElasticityTest3, Displacement3D)
   initialize_parameters();
   ParameterAcceptor::prm.parse_input_from_string(
     R"(
-subsection Immersed Problem
-  set Dirichlet boundary ids             = 0
-  set Initial refinement                 = 3
-  set Normal flux boundary ids           = 1,2
-  set Output name                        = Displacement3D
-  subsection Grid generation
-    set Domain type              = generate
-    set Grid generator           = subdivided_cylinder
-    set Grid generator arguments = 2:2:2
-  end
-  subsection Immersed inclusions
-    set Inclusions file                     = ../data/tests/cylinder_x.txt
-    set Reference inclusion data            = 0,0,0,0.1,0,0,0,0.1,0
-    set Inclusions refinement               = 50
-    set Number of fourier coefficients      = 2
-    set Selection of Fourier coefficients   = 3,7
-  end
-  subsection Material properties
-    set Material tags by material id =
-    subsection default
-      set Lame lambda     = 50.0
-      set Lame mu         = 2.0
+    subsection Immersed Problem
+      set Dirichlet boundary ids             = 0
+      set Initial refinement                 = 3
+      set Normal flux boundary ids           = 1,2
+      set Output name                        = Displacement3D
+      subsection Grid generation
+        set Domain type              = generate
+        set Grid generator           = subdivided_cylinder
+        set Grid generator arguments = 2:2:2
+      end
+      subsection Immersed inclusions
+        set Inclusions file                     = ../data/tests/cylinder_x.txt
+        set Reference inclusion data            = 0,0,0,0.1,0,0,0,0.1,0
+        set Inclusions refinement               = 50
+        set Number of fourier coefficients      = 2
+        set Selection of Fourier coefficients   = 3,7
+      end
+      subsection Material properties
+        set Material tags by material id =
+        subsection default
+          set Lame lambda     = 50.0
+          set Lame mu         = 2.0
+        end
+      end
     end
-  end
-end
-subsection Solvers
-  subsection Augmented Lagrange
-    set Log frequency = 1
-    set Log history   = false
-    set Log result    = true
-    set Max steps     = 1000
-    set Reduction     = 1.e-10
-    set Tolerance     = 1.e-10
-  end
-  subsection Displacement
-    set Log frequency = 1
-    set Log history   = false
-    set Log result    = true
-    set Max steps     = 1000
-    set Reduction     = 1.e-8
-    set Tolerance     = 1.e-10
-  end
-  subsection Reduced mass
-    set Log frequency = 1
-    set Log history   = false
-    set Log result    = true
-    set Max steps     = 100
-    set Reduction     = 1.e-10
-    set Tolerance     = 1.e-12
-  end
-end
+    subsection Solvers
+      subsection Augmented Lagrange
+        set Log frequency = 1
+        set Log history   = false
+        set Log result    = true
+        set Max steps     = 1000
+        set Reduction     = 1.e-10
+        set Tolerance     = 1.e-10
+      end
+      subsection Displacement
+        set Log frequency = 1
+        set Log history   = false
+        set Log result    = true
+        set Max steps     = 1000
+        set Reduction     = 1.e-8
+        set Tolerance     = 1.e-10
+      end
+      subsection Reduced mass
+        set Log frequency = 1
+        set Log history   = false
+        set Log result    = true
+        set Max steps     = 100
+        set Reduction     = 1.e-10
+        set Tolerance     = 1.e-12
+      end
+    end
     )");
 
   ParameterAcceptor::parse_all_parameters();
@@ -408,4 +408,53 @@ end
   const double tol = 1e-4;
   ASSERT_NEAR(problem.solution.block(0).l2_norm(), 3.6763217, tol);
   ASSERT_NEAR(problem.solution.block(1).l2_norm(), 81.839722544, tol);
+}
+
+TEST(ElasticityTest, ExactLambda)
+{
+  ParameterAcceptor::clear();
+  static constexpr int             dim = 2;
+  ElasticityProblemParameters<dim> par;
+  get_default_test_parameters(par);
+  ElasticityProblem<dim> problem(par);
+
+
+  initialize_parameters();
+  ParameterAcceptor::prm.parse_input_from_string(
+    R"(
+    subsection Functions
+      subsection Exact solution
+        set Function constants  = ri=0.2, re=1, di=0.1
+        set Function expression = if((x^2+y^2) > (ri*ri), ri*di / (re*re-ri*ri) * (re*re*x/(x^2+y^2) - x), di *x/ri); if((x^2+y^2) > (ri*ri), ri*di / (re*re-ri*ri) * (re*re*y/(x^2+y^2) - y), di*y/ri)
+        set Variable names      = x,y,t
+        set Weight expression   = if((x^2+y^2) > 0.04, 1., 0.)
+      end
+    end
+    subsection Immersed Problem
+      set Dirichlet boundary ids             = 0
+      set FE degree                          = 1
+      set Initial refinement                 = 3
+      set Output name                        = Exact
+      subsection Grid generation
+        set Domain type              = cylinder
+        set Grid generator           = cylinder
+        set Grid generator arguments = 1.
+      end
+      subsection Immersed inclusions
+        set Reference inclusion data            = 0, 0, 0.1, 0, 0, 0.1
+        set Inclusions                          = 0,0,0.2
+        set Number of fourier coefficients      = 2
+        set Selection of Fourier coefficients   = 2,5
+      end
+    end
+    )");
+
+  ParameterAcceptor::parse_all_parameters();
+  problem.run();
+
+  const double tol = 1e-4;
+  // ASSERT_NEAR(problem.solution.block(0).l2_norm(), 3.6763217, tol);
+  // ASSERT_NEAR(problem.solution.block(1).l2_norm(), 81.839722544, tol);
+  std::cout << problem.solution.block(0).l2_norm() << std::endl;
+  std::cout << problem.solution.block(1).l2_norm() << std::endl;
 }
